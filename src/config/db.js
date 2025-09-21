a91165f9f5dc72d92072e794d877f9df3132522f
SerdarAkbulut <AkbulutSerdar@outlook.com.tr>
2025-09-21 14:21:35 +0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,22 +2,25 @@ const { Sequelize } = require("sequelize");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
+const sequelize = new Sequelize(
+  "postgresql://postgres.iofgthwhmfcjlezhczja:B0eczGBMpbYBhKzw@aws-1-eu-north-1.pooler.supabase.com:5432/postgres",
+  {
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
-  },
-  pool: {
-    max: 2,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-});
+    pool: {
+      max: 2,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
 
 if (process.env.NODE_ENV !== "production") {
   sequelize
