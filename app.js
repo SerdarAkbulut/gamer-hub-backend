4e4a444dded9f386786166dbb1cec301289cf5bc
SerdarAkbulut <AkbulutSerdar@outlook.com.tr>
2025-09-21 12:51:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require("express");
-// const dotenv = require("dotenv");
-// const sequelize = require("./src/startup/db.js");
-// require("./src/models/index.js");
+const dotenv = require("dotenv");
+const sequelize = require("./src/startup/db.js");
+require("./src/models/index.js");
 
-// dotenv.config();
+dotenv.config();
 const app = express();
 app.use(express.json());
-// require("./src/startup/routers")(app);
+require("./src/startup/routers")(app);
 
-// sequelize
-//   .authenticate()
-//   .then(() => console.log("✅ Veritabanı bağlantısı başarılı."))
-//   .catch((err) => console.error("❌ Bağlantı hatası:", err));
+sequelize
+  .authenticate()
+  .then(() => console.log("✅ Veritabanı bağlantısı başarılı."))
+  .catch((err) => console.error("❌ Bağlantı hatası:", err));
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`🚀 Sunucu ${PORT} portunda çalışıyor`));
